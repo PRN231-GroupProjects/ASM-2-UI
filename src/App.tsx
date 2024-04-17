193a8c5886197880e245030f2091eb415b62d10c
adiosNTV <nguyenthienvu12102003@gmail.com>
2024-04-17 11:17:24 +0700

    feat: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import OrdersTable from "./components/OrdersTable";
+import Layout from "./layout";
+
+const App = () => {
+	return (
+		<Layout>
+			<OrdersTable />
+		</Layout>
+	);
+};
+
+export default App;
